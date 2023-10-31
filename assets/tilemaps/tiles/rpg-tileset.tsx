fd3c9312927a8911d50d4701bbd62c90c706298e
jie_w <jie_wu@asus.com>
2023-10-31 08:28:00 +0000

    git init

---
+++ assets/tilemaps/tiles/rpg-tileset.tsx
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="rpg-tileset" tilewidth="16" tileheight="16" tilecount="10000" columns="100">
+ <image source="rpg-tileset.png" width="1600" height="1600"/>
+ <wangsets>
+  <wangset name="River Ground" type="corner" tile="1301">
+   <wangcolor name="water" color="#ff0000" tile="1407" probability="1"/>
+   <wangcolor name="stone-left" color="#00ff00" tile="1401" probability="1"/>
+   <wangcolor name="stone-right" color="#0000ff" tile="1404" probability="1"/>
+   <wangcolor name="stone-bottom" color="#ff7700" tile="1603" probability="1"/>
+   <wangcolor name="stone-up" color="#00e9ff" tile="1203" probability="1"/>
+   <wangcolor name="dirt" color="#ff00d8" tile="2023" probability="1"/>
+   <wangtile tileid="1202" wangid="0,0,0,5,0,5,0,0"/>
+   <wangtile tileid="1203" wangid="0,0,0,5,0,5,0,0"/>
+   <wangtile tileid="1301" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="1302" wangid="0,5,0,0,0,0,0,5"/>
+   <wangtile tileid="1303" wangid="0,5,0,0,0,0,0,5"/>
+   <wangtile tileid="1304" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="1401" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="1402" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1403" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1404" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="1412" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1413" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1417" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1418" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1422" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1423" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1427" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1428" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1501" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="1502" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1503" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1504" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="1512" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1513" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1522" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1523" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1601" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="1602" wangid="0,4,0,0,0,0,0,4"/>
+   <wangtile tileid="1603" wangid="0,4,0,0,0,0,0,4"/>
+   <wangtile tileid="1604" wangid="0,0,0,0,0,0,0,3"/>
+   <wangtile tileid="1921" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="1922" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="1923" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="1924" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2021" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2022" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2023" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2024" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2121" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2122" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2123" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2124" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2221" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2222" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2223" wangid="0,6,0,6,0,6,0,6"/>
+   <wangtile tileid="2224" wangid="0,6,0,6,0,6,0,6"/>
+  </wangset>
+ </wangsets>
+</tileset>
