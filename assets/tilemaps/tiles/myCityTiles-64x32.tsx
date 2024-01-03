7dc399fa9132a5e8b2760651c7a7899250b273a8
jie_w <jie_wu@asus.com>
2024-01-03 17:52:01 +0800

    update scene city scene test

---
+++ assets/tilemaps/tiles/myCityTiles-64x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="myCityTiles-64x32" tilewidth="64" tileheight="32" tilecount="165" columns="11">
+ <image source="myCityTiles-64x32.png" width="704" height="480"/>
+</tileset>
